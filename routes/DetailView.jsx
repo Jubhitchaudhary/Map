863ef3ff591d8427ae81c983e7c1e53b92e47d48
Jubhit <JUBHITCHAUDHARY6@GMAIL.COM>
2024-04-04 23:31:41 -0700

    ok

---
+++ routes/DetailView.jsx
@@ -0,0 +1,8 @@
+import ExhibitionDetail from "../Marvel/src/components/ExhibitionDetail";
+const DetailView = () => {
+    return (
+        <ExhibitionDetail/>
+    );
+}
+
+export default DetailView;
---
+++ routes/Layout.jsx
@@ -0,0 +1,21 @@
+import {Outlet, Link} from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li className="home-link" key="home-button">
+                        <Link style={{color: "#c65f5f"}} to="/">Home</Link> 
+                        {/** use Link so the button actually link to the homepage */}
+                    </li>
+                </ul>
+            </nav>
+            <Outlet/>{/** Dont really get it but will comeback */}
+
+        </div>
+    )
+}
+
+export default Layout;
+
